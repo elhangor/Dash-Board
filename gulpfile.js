91aafff5fbfb17d94d8495f8b657c0f1b9d29cf8
Ahmed Elhangor <ahmed.elhangor88@gmail.com>
2019-12-17 00:09:40 +0200

    0

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,45 @@
-var gulp = require('gulp'),
-		concat = require('gulp-concat'),
-		prefix = require('gulp-autoprefixer'),
-		sass = require('gulp-sass'),
-		pug = require('gulp-pug'),
-		livereload = require('gulp-livereload'),
-		sourcemaps = require('gulp-sourcemaps'),
-        uglify = require('gulp-minify');
-        
-gulp.task('html',async function buildHTML(){
-    return gulp.src("stage/html/*.pug")
-        .pipe(pug({pretty:true}))
-        .pipe(gulp.dest('dist'))
-        .pipe(livereload())
+var gulp = require("gulp"),
+  concat = require("gulp-concat"),
+  prefix = require("gulp-autoprefixer"),
+  sass = require("gulp-sass"),
+  pug = require("gulp-pug"),
+  livereload = require("gulp-livereload"),
+  sourcemaps = require("gulp-sourcemaps"),
+  uglify = require("gulp-minify");
+
+gulp.task("html", async function buildHTML() {
+  return gulp
+    .src("stage/html/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("dist"))
+    // .pipe(livereload());
 });
 
-gulp.task('css',async function() {
-	return gulp.src("stage/css/**/*.css" , "stage/css/**/*.scss")
-		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(prefix('last 5 versions'))
-		.pipe(concat('main.css'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist/css'))
-		.pipe(livereload())
+gulp.task("css", async function() {
+  return gulp
+    .src("stage/css/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .pipe(prefix("last 5 versions"))
+    .pipe(concat("main.css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(livereload());
 });
 
-gulp.task('js',async function() {
-	return gulp.src("stage/js/*.js")
-		.pipe(concat('main.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'))
-		.pipe(livereload())
+gulp.task("js", async function() {
+  return gulp
+    .src("stage/js/*.js")
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"))
+    .pipe(livereload());
 });
 
-gulp.task('watch', function() {
-	require('./server.js');
-	livereload.listen();
-	gulp.watch('stage/pug/*.pug',  gulp.series('html'));
-	gulp.watch('stage/css/*.scss',  gulp.series('css'));
-	gulp.watch('stage/js/*.js', gulp.series('js'));
+gulp.task("watch", function() {
+  require("./server.js");
+  livereload.listen();
+  gulp.watch("stage/pug/*.pug", gulp.series("html"));
+  gulp.watch("stage/css/*.scss", gulp.series("css"));
+  gulp.watch("stage/js/*.js", gulp.series("js"));
 });
