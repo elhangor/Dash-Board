f77dacabadcf3866ebc8c6a5ababab397aefc9f3
Ahmed Elhangor <ahmed.elhangor88@gmail.com>
2019-12-18 00:08:35 +0200

    L 10 - 20

--- stage/js/main.js
+++ stage/js/main.js
@@ -1,6 +1,50 @@
 $(function(){
   'use strict'
+  // Show & Hide Slide Bar
   $('.toggle-sidebar').on('click',function(){
     $(".content-area, .sidebar").toggleClass("no-sidebar");
   });
+  // Toggle Sub Menu
+  $('.toggle-submenu').on('click',function(){
+    $(this).find(".fa-angle-right").toggleClass('down');
+    $(this).next(".child-link").slideToggle();
+  });
+  // Toggle Full Screen
+  $('.full-screen').on('click',function(){
+    $(this).toggleClass("f-screen");
+    if($(this).hasClass('f-screen')){
+      openFullscreen();
+    }else{
+      closeFullscreen();
+    }
+  });
+  
 });
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
